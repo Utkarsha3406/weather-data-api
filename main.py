69f945d53987a8a329fd3d5b981ed675432fcbdb
Utkarsha3406 <up3406@srmist.edu.in>
2024-01-22 23:16:23 +0530

    Initial commit,made changes for main website look

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask,render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
